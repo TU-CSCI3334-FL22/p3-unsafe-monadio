d5c508a58993c509d3670a6b65a76e22dd54be58
ymatsumo23 <ymatsumo@trinity.edu>
2022-10-26 14:18:35 -0500

    skeleton for follow table, need fix point

--- Code/LLGen.hs
+++ Code/LLGen.hs
@@ -1,6 +1,7 @@
 module Code.LLGen where
 import           Code.Reader
-import           Data.Map.Strict (Map)
+import           Data.Map.Strict (Map,(!), fromList)
+import qualified Data.Map.Strict as Map
 import           Code.Grammar (GrammarAST (..), Rhs (..), Lhs (..), ProductSet (..), NonTerminal)
 import Data.List
 
@@ -22,7 +23,34 @@ makeFirstTable = undefined
 
 
 makeFollowTable :: (GrammarAST, [NonTerminal]) -> FirstTable -> FollowTable
-makeFollowTable = undefined
+makeFollowTable (AST ast, nt) first_table = undefined
+  where
+    init_map = (fromList $ [(nt', []) | nt' <- nt])
+    (GrammarProductionSet (Lhs top_level) lst) = head ast
+    newLst = Map.insert top_level ["eof"] init_map
+    productions = getRules (AST ast)
+    newFollowTable = foldl (\t p -> followRule p first_table t nt) newLst productions
+
+
+
+followRule :: (Lhs, Rhs) -> FirstTable -> FollowTable -> [NonTerminal] -> FollowTable
+followRule (Lhs lhs, Rhs rhs) first_table follow_table nt = newFollowTable
+  where 
+    trailer = case Map.lookup lhs follow_table of
+              Nothing -> error "aaaaaaaaaaa"
+              Just lst -> lst
+    newFollowTable = fst $ foldr (\x b -> followAux x (snd b) first_table (fst b) nt) (follow_table, trailer) rhs
+
+followAux :: String -> [String] -> FirstTable -> FollowTable -> [NonTerminal] -> (FollowTable, [String])
+followAux rhs trailer first_table follow_table nt = if rhs `elem` nt then let newTable = Map.insertWith (++) rhs trailer follow_table
+                                                                              newTrailer = if "" `elem` firstB then trailer ++ filter (/="") firstB else firstB 
+                                                                              in (newTable, newTrailer) 
+                                                    else  (follow_table, firstB)
+  where
+    firstB = case Map.lookup rhs first_table of 
+                  Nothing -> error "???"
+                  Just lst -> lst
+
 
 makeNextTable :: (GrammarAST, [NonTerminal]) -> FirstTable -> FollowTable -> NextTable
 makeNextTable (ast,lst) fstTbl followTbl = 
@@ -41,11 +69,11 @@ nextOfRule (Lhs a) (Rhs lst) fstTbl followTbl =
        (a, firstBs ++ (lookUpVal a followTbl))
      else (a,firstBs)
 
-lookUpVal :: Eq a => a -> [(a,v)] ->  v
--- lookUpVal k [] = Nothing
-lookUpVal k (x:xs) = 
-  if fst x == k then snd x -- Just (snd x)
-  else lookUpVal k xs
+lookUpVal :: (Eq a, Ord a) => a -> Map a v ->  v
+lookUpVal = flip (!)
+-- lookUpVal k (x:xs) = 
+--   if fst x == k then snd x -- Just (snd x)
+--   else lookUpVal k xs
 
 
 
